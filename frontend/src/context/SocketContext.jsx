3e925bed959ac7a8e6f2a01cc4f95cc97d66b04e
AyushKumarThakur21BCE3771 <144116323+AyushKumarThakur21BCE3771@users.noreply.github.com>
2024-08-23 17:52:57 +0530

    socket url changed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-trfj.onrender.com/", {
+			const socket = io("https://main--chattie-chat-app.netlify.app/", {
 				query: {
 					userId: authUser._id,
 				},
